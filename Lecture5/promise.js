ff87ad7ff69efe85f44450012371fd42c0590f80
thatitakhumsap-6606022410014 <138425547+thatitakhumsap-6606022410014@users.noreply.github.com>
2024-07-31 09:21:54 +0700

    3

--- Lecture5/promise.js
+++ Lecture5/promise.js
@@ -1,21 +1,23 @@
 function promiseTimeout(ms) {
-    return new Promise((remove, reject) => {
+    return new Promise((resolve, reject) => {
         setTimeout(resolve, ms);
     });
 }
 
+console.log('Start');
 promiseTimeout(2000)
 .then(() => {
     console.log("Done!!");
     return promiseTimeout(1000);
 })
 .then(() => {
-    console.log("Also done!!");
+    console.log("Also Done!!");
     return Promise.resolve(42);
 })
 .then((result) => {
     console.log(result);
 })
 .catch(() => {
-    console.log("Error!")
-})
+    console.log("Error");
+});
+console.log('End');
